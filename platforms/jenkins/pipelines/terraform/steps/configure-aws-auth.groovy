a5c7f8dc1063b6faeaff3b03ddf5c6948046c8a5
armck <armck.phantom@gmail.com>
2024-12-20 03:36:51 +0000

    fix auth step escape

--- platforms/jenkins/pipelines/terraform/steps/configure-aws-auth.groovy
+++ platforms/jenkins/pipelines/terraform/steps/configure-aws-auth.groovy
@@ -1,8 +1,8 @@
 def call() {
     sh """
         # configure CICD agent to assume the iac-deployer role in the associated account
-        export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
-            $(aws sts assume-role \
+        export \$(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
+            \$(aws sts assume-role \
             --role-arn "arn:aws:iam::${AWS_ACCOUNT_ID}:role/iac-deployer" \
             --role-session-name terraform \
             --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
